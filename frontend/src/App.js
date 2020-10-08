2aa96a0aaa2c02cdb8ccc7fedc013b6decc1b7d4
KHALFAOUI Anis <anis.khalfaoui@soprahr.com>
2020-10-08 22:10:44 +0100

    implement the products and products/:id routes; design the mongodb database and connect it to the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-App = () => {
+//screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello Proshop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <Fragment>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col id={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
